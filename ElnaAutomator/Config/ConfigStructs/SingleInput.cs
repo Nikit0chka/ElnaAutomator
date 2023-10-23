01c3ab3221966b89878beb720262f5ae9447d98e
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:25:08 +0400

    Created SingleInput.cs

---
+++ ElnaAutomator/Config/ConfigStructs/SingleInput.cs
@@ -0,0 +1,8 @@
+﻿namespace ElnaAutomator.Config.ConfigStructs;
+
+public class SingleInput
+{
+    public required string Name { get; set; }
+    public required DiscreteInput DiscreteInput { get; set; }
+    public required int Address { get; set; }
+}
