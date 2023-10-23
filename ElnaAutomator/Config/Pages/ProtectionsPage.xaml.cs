066d42e16a52748c374820a5a26ad4f1a39d8d49
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:24:59 +0400

    Created ProtectionsPage.xaml.cs

---
+++ ElnaAutomator/Config/Pages/ProtectionsPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ElnaAutomator.Config.Pages;
+
+public partial class ProtectionsPage : Page
+{
+    public ProtectionsPage()
+    {
+        InitializeComponent();
+    }
+}
