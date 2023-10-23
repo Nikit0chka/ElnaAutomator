d45e102955562024804debaf4904b9289a9ebbb0
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 17:37:44 +0400

    Created ExcelReader.cs

---
+++ ElnaAutomator/Config/ExcelReader.cs
@@ -0,0 +1,11 @@
+namespace ElnaAutomator.Config;
+
+public class ExcelReader
+{
+    private string _pathToFile;
+
+    public ExcelReader(string pathToFile)
+    {
+        _pathToFile = pathToFile;
+    }
+}
