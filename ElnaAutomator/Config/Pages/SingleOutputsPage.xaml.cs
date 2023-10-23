1b337a8fc92677bcfe72a0020f3b9766e541d720
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:25:44 +0400

    Created SingleOutputsPage.xaml.cs

---
+++ ElnaAutomator/Config/Pages/SingleOutputsPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ElnaAutomator.Pages;
+
+public partial class SingleOutputsPage : Page
+{
+    public SingleOutputsPage()
+    {
+        InitializeComponent();
+    }
+}
