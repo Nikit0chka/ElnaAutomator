5c0eb5d1dbb967e625a25201292bd796b4cd154e
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:25:29 +0400

    Created SingleOutput.cs

---
+++ ElnaAutomator/Config/ConfigStructs/SingleOutput.cs
@@ -0,0 +1,9 @@
+﻿using ElnaAutomator.Config.ConfigStructs;
+
+namespace ElnaAutomator.ConfigStructs;
+
+public class SingleOutput
+{
+    public required string Name { get; set; }
+    public DiscreteOutput? DigitalOutput { get; set; }
+}
