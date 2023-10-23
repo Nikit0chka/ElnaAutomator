c2c7f2f1081f99690fbbead908f0f02437623071
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:24:45 +0400

    Created MainWindow.xaml.cs

---
+++ ElnaAutomator/Config/Windows/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using ElnaAutomator.Config.Pages;
+using ElnaAutomator.Pages;
+
+namespace ElnaAutomator.Config.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    private readonly ConfigMainPage _configMainPage;
+
+    public MainWindow()
+    {
+        _configMainPage = new ConfigMainPage();
+        InitializeComponent();
+    }
+    
+    private void ShowConfigButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        MainFrame.Navigate(_configMainPage);
+    }
+}
