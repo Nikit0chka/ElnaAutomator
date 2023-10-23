5e4bb742293c0c7014d843fe93289d8748cb3f3d
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 23:42:35 +0400

    made the fields HighLimit LowLimit required

--- ElnaAutomator/Config/ConfigStructs/AnalogInput.cs
+++ ElnaAutomator/Config/ConfigStructs/AnalogInput.cs
@@ -1,10 +1,10 @@
-﻿namespace ElnaAutomator.ConfigStructs;
+﻿namespace ElnaAutomator.Config.ConfigStructs;
 
 public class AnalogInput
 {
     public required string Name{ get; set; }
-    public int? HighLimit{ get; set; }
-    public int? LowLimit{ get; set; }
+    public required int HighLimit{ get; set; }
+    public required int LowLimit{ get; set; }
     public int? HighAlarm{ get; set; }
     public int? LowAlarm{ get; set; }
     public int? HighWarning{ get; set; }
