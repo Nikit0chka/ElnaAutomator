830feb9b841db1914a88b1a2751cf2831fc19aed
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:23:28 +0400

    Created DiscreteOutputsPage.xaml.cs

---
+++ ElnaAutomator/Config/Pages/DiscreteOutputsPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ElnaAutomator.Config.Pages;
+
+public partial class DiscreteOutputsPage : Page
+{
+    public DiscreteOutputsPage()
+    {
+        InitializeComponent();
+    }
+}
