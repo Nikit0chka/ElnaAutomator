d8bc47b0fbe43f74576237c07301e2570b4ef2fe
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:20:07 +0400

    Created AnalogSignalProtection.cs

---
+++ ElnaAutomator/Config/ConfigStructs/AnalogSignalProtection.cs
@@ -0,0 +1,8 @@
+﻿namespace ElnaAutomator.ConfigStructs;
+
+public class AnalogSignalProtection
+{
+    public required string Name { get; set; }
+    public required AnalogInput AnalogInput { get; set; }
+    public required bool IsUpperLimitProtection { get; set; }
+}
