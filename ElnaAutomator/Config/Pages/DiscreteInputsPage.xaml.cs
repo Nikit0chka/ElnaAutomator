bf03e006c536921cb21ced9a924efb460a6bb17c
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:22:59 +0400

    Created DiscreteInputsPage.xaml.cs

---
+++ ElnaAutomator/Config/Pages/DiscreteInputsPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ElnaAutomator.Config.Pages;
+
+public partial class DiscreteInputsPage : Page
+{
+    public DiscreteInputsPage()
+    {
+        InitializeComponent();
+    }
+}
