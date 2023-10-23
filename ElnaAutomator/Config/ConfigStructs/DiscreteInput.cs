c51973f159f9b43ada6ecf0be4ff199d829a22b5
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:22:40 +0400

    Created DiscreteInput.cs

---
+++ ElnaAutomator/Config/ConfigStructs/DiscreteInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ElnaAutomator.ConfigStructs;
+
+namespace ElnaAutomator.Config.ConfigStructs;
+
+public class DiscreteInput
+{
+    public required string Name { get; set; }
+    public List<SingleInput>? SingleInput{ get; set; }
+}
