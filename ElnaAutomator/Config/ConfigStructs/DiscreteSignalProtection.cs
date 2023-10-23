8d19dfdbd3c8672967325d0e03f8203b810606e7
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:23:37 +0400

    Created DiscreteSignalProtection.cs

---
+++ ElnaAutomator/Config/ConfigStructs/DiscreteSignalProtection.cs
@@ -0,0 +1,10 @@
+﻿using ElnaAutomator.Config.ConfigStructs;
+
+namespace ElnaAutomator.ConfigStructs;
+
+public class DiscreteSignalProtection
+{
+    public required string Name{ get; set; }
+    public required SingleInput SingleInput { get; set; }
+    public required bool IsOnLimitProtection { get; set; }
+}
