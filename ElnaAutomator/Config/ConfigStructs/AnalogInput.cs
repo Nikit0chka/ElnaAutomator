99e527be003065a37a7bdfa2a8bf67be5d2643ec
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:19:30 +0400

    Created AnalogInput.cs.

---
+++ ElnaAutomator/Config/ConfigStructs/AnalogInput.cs
@@ -0,0 +1,12 @@
+﻿namespace ElnaAutomator.ConfigStructs;
+
+public class AnalogInput
+{
+    public required string Name{ get; set; }
+    public int? HighLimit{ get; set; }
+    public int? LowLimit{ get; set; }
+    public int? HighAlarm{ get; set; }
+    public int? LowAlarm{ get; set; }
+    public int? HighWarning{ get; set; }
+    public int? LowWarning { get; set; }
+}
