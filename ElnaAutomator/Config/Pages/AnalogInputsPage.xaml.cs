49ece9092d15e488a40e2ee76c9c0f4894677a26
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-23 08:19:58 +0400

    Created AnalogInputsPage.xaml.cs

---
+++ ElnaAutomator/Config/Pages/AnalogInputsPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ElnaAutomator.Pages;
+
+public partial class AnalogInputsPage : Page
+{
+    public AnalogInputsPage()
+    {
+        InitializeComponent();
+    }
+}
