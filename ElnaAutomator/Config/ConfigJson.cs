f9eeb10cda3e7efa1d2720e93bbccbec31a1e63d
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-27 16:10:37 +0400

    Created class for working with config by json

---
+++ ElnaAutomator/Config/ConfigJson.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using ElnaAutomator.Config.ConfigStructs;
+
+namespace ElnaAutomator.Config.Windows;
+
+/// <summary>
+/// Logic for writing/reading config from json
+/// </summary>
+public class ConfigJson
+{
+    public required List<AnalogInput> AnalogInputs { get; set; }
+    public required List<AnalogSignalProtection> AnalogSignalProtections { get; set; }
+    public required List<DiscreteInput> DiscreteInputs { get; set; }
+    public required List<DiscreteOutput> DiscreteOutputs { get; set; }
+    public required List<DiscreteSignalProtection> DiscreteSignalProtections { get; set; }
+    public required List<ExecutiveMechanism> ExecutiveMechanisms { get; set; }
+    public required List<SingleInput> SingleInputs { get; set; }
+    public required List<SingleOutput> SingleOutputs { get; set; }
+}
