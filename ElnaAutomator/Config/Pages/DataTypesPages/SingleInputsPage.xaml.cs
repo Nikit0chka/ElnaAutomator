dc494fa5e9f52eab18b80089149b0b95f742cb97
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-29 02:11:00 +0400

    New page for working with single signals

---
+++ ElnaAutomator/Config/Pages/DataTypesPages/SingleInputsPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ElnaAutomator.Config.Pages.DataTypesPages;
+
+public partial class SingleInputsPage : Page
+{
+    public SingleInputsPage()
+    {
+        InitializeComponent();
+    }
+}
