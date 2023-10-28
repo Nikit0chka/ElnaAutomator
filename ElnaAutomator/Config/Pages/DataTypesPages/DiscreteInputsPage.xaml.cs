5bce7b937e50ea2b4c183e42737d5d7b9c462e92
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-10-29 02:06:07 +0400

    Added logic for working with signals by data grid

--- ElnaAutomator/Config/Pages/DataTypesPages/DiscreteInputsPage.xaml.cs
+++ ElnaAutomator/Config/Pages/DataTypesPages/DiscreteInputsPage.xaml.cs
@@ -1,11 +1,51 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using ElnaAutomator.Config.ConfigStructs;
 
 namespace ElnaAutomator.Config.Pages.DataTypesPages;
 
 public partial class DiscreteInputsPage : Page
 {
+    private readonly App? _currentApp;
     public DiscreteInputsPage()
     {
         InitializeComponent();
+        _currentApp = Application.Current as App;
+        DiDataGrid.ItemsSource = _currentApp?.DiscreteInputs;
+    }
+
+    private void AddDi_OnClick(object sender, RoutedEventArgs e)
+    {
+        var discreteInput = new DiscreteInput()
+        {
+            Name = $"Di{_currentApp?.DiscreteInputs.Count}",
+        };
+        _currentApp?.DiscreteInputs.Add(discreteInput);
+        DiDataGrid.Items.Refresh();
+    }
+
+    private void DeleteDi_OnClick(object sender, RoutedEventArgs e)
+    {
+        List<object> selectedItems = new();
+
+        foreach (var item in DiDataGrid.Items)
+        {
+            var row = (DataGridRow) DiDataGrid.ItemContainerGenerator.ContainerFromItem(item);
+            if (row == null || DiDataGrid.Columns[0] is not DataGridCheckBoxColumn checkBox)
+                continue;
+
+            var element = checkBox.GetCellContent(row);
+            if (element is CheckBox { IsChecked: true })
+                selectedItems.Add(item);
+        }
+
+        selectedItems.ForEach(c =>
+        {
+            if (c is DiscreteInput discreteInput)
+                _currentApp?.DiscreteInputs.Remove(discreteInput);
+        });
+
+        DiDataGrid.Items.Refresh();
     }
 }
