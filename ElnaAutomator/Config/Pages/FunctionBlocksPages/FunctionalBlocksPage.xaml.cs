b3b593f5f94b7357d28754515af2dc1ce103b6d4
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-01 17:47:23 +0400

    Created for working with functional blocks

---
+++ ElnaAutomator/Config/Pages/FunctionBlocksPages/FunctionalBlocksPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ElnaAutomator.Config.Pages.FunctionBlocksPages;
+
+public partial class FunctionalBlocksPage : Page
+{
+    public FunctionalBlocksPage()
+    {
+        InitializeComponent();
+    }
+}
