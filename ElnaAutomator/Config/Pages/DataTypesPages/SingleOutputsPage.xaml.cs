c789fabd612f4d90d4420059a1be98829b97171e
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-02 17:43:47 +0400

    Added page for working with singleOutputs

---
+++ ElnaAutomator/Config/Pages/DataTypesPages/SingleOutputsPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using ElnaAutomator.Config.ConfigStructs;
+
+namespace ElnaAutomator.Config.Pages.DataTypesPages;
+
+public partial class SingleOutputsPage : Page
+{
+    private readonly App? _currentApp;
+
+    public SingleOutputsPage()
+    {
+        InitializeComponent();
+        _currentApp = Application.Current as App;
+        SingleOutputsDataGrid.ItemsSource = _currentApp?.SingleOutputs;
+        DiscreteOutputsCmbBx.ItemsSource = _currentApp?.DiscreteOutputs;
+    }
+
+    private void AddSingleOutput_OnClick(object sender, RoutedEventArgs e)
+    {
+        var singleOutput = new SingleOutput()
+        {
+            Name = $"SO{_currentApp?.SingleOutputs.Count}",
+        };
+
+        _currentApp?.SingleOutputs.Add(singleOutput);
+        SingleOutputsDataGrid.Items.Refresh();
+    }
+
+    private void DeleteSingleOutput_OnClick(object sender, RoutedEventArgs e)
+    {
+        List<object> selectedItems = new();
+
+        foreach (var item in SingleOutputsDataGrid.Items)
+        {
+            var row = (DataGridRow)SingleOutputsDataGrid.ItemContainerGenerator.ContainerFromItem(item);
+            if (row == null || SingleOutputsDataGrid.Columns[0] is not DataGridCheckBoxColumn checkBox)
+                continue;
+
+            var element = checkBox.GetCellContent(row);
+            if (element is CheckBox { IsChecked: true })
+                selectedItems.Add(item);
+        }
+
+        selectedItems.ForEach(c =>
+        {
+            if (c is SingleOutput singleOutput)
+                _currentApp?.SingleOutputs.Remove(singleOutput);
+        });
+
+        SingleOutputsDataGrid.Items.Refresh();
+    }
+}
