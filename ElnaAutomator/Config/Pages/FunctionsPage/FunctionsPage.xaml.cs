9d90371b3a198dca101c5e745bd2cc18312290ca
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-02 17:42:31 +0400

    Added event holder on button click

--- ElnaAutomator/Config/Pages/FunctionsPage/FunctionsPage.xaml.cs
+++ ElnaAutomator/Config/Pages/FunctionsPage/FunctionsPage.xaml.cs
@@ -1,9 +1,21 @@
-﻿namespace ElnaAutomator.Config.Pages.FunctionsPage;
+﻿using System.Windows;
+using ElnaAutomator.Config.Generators;
+
+namespace ElnaAutomator.Config.Pages.FunctionsPage;
 
 public partial class FunctionsPage
 {
+    private readonly App? _currentApp;
+
     public FunctionsPage()
     {
         InitializeComponent();
+        _currentApp = Application.Current as App;
+    }
+
+    private void AnyAnalogPsFunc_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (_currentApp != null)
+            FunctionsGenerator.GenerateAnyAnalogPs(_currentApp.PathToProject, _currentApp.AnalogInputs);
     }
 }
