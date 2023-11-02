521733e25ddf3a1c60571df0b2ddf3d7d33abc0b
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-02 17:37:24 +0400

    Made field "AnalogInput" nullable

--- ElnaAutomator/Config/ConfigStructs/AnalogSignalProtection.cs
+++ ElnaAutomator/Config/ConfigStructs/AnalogSignalProtection.cs
@@ -3,6 +3,6 @@
 public class AnalogSignalProtection
 {
     public required string Name { get; set; }
-    public required AnalogInput AnalogInput { get; set; }
+    public  AnalogInput? AnalogInput { get; set; }
     public required bool IsUpperLimitProtection { get; set; }
 }
