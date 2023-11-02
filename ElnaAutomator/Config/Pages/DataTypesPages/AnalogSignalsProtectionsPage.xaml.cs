bb59d2be2618d1b56f7b14e6da412cea0b30f7d1
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-02 17:38:09 +0400

    Added event on buttonsClick

--- ElnaAutomator/Config/Pages/DataTypesPages/AnalogSignalsProtectionsPage.xaml.cs
+++ ElnaAutomator/Config/Pages/DataTypesPages/AnalogSignalsProtectionsPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using ElnaAutomator.Config.ConfigStructs;
 
 namespace ElnaAutomator.Config.Pages.DataTypesPages;
 
@@ -10,8 +12,42 @@ public partial class AnalogSignalsProtectionsPage
     {
         InitializeComponent();
         _currentApp = Application.Current as App;
-        AiProtectionsDataGrid.ItemsSource = _currentApp?.AnalogInputs;
-        var asd = AiProtectionsDataGrid.FindName("AnalogSignalCmbBBx") as ComboBox;
-        asd!.ItemsSource = _currentApp?.AnalogInputs;
+        AiProtectionsDataGrid.ItemsSource = _currentApp?.AnalogSignalProtections;
+        AnalogSignalsCmbBx.ItemsSource = _currentApp?.AnalogInputs;
+    }
+
+    private void AddAiProtection_OnClick(object sender, RoutedEventArgs e)
+    {
+        var newAnalogSignalProtection = new AnalogSignalProtection()
+        {
+            Name = $"AiP{_currentApp?.AnalogSignalProtections.Count}",
+            IsUpperLimitProtection = false,
+        };
+        _currentApp?.AnalogSignalProtections.Add(newAnalogSignalProtection);
+        AiProtectionsDataGrid.Items.Refresh();
+    }
+
+    private void DeleteAiProtection_OnClick(object sender, RoutedEventArgs e)
+    {
+        List<object> selectedItems = new();
+
+        foreach (var item in AiProtectionsDataGrid.Items)
+        {
+            var row = (DataGridRow) AiProtectionsDataGrid.ItemContainerGenerator.ContainerFromItem(item);
+            if (row == null || AiProtectionsDataGrid.Columns[0] is not DataGridCheckBoxColumn checkBox)
+                continue;
+
+            var element = checkBox.GetCellContent(row);
+            if (element is CheckBox { IsChecked: true })
+                selectedItems.Add(item);
+        }
+
+        selectedItems.ForEach(c =>
+        {
+            if (c is AnalogSignalProtection analogSignalProtection)
+                _currentApp?.AnalogSignalProtections.Remove(analogSignalProtection);
+        });
+
+        AiProtectionsDataGrid.Items.Refresh();
     }
 }
