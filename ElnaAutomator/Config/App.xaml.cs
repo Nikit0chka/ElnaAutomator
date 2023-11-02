1697da34e9b45520fd13706222d47593b50a5547
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-02 17:38:36 +0400

    Added new ai. Changed start file choose logic

--- ElnaAutomator/Config/App.xaml.cs
+++ ElnaAutomator/Config/App.xaml.cs
@@ -22,57 +22,71 @@ public partial class App
     public List<AnalogSignalProtection> AnalogSignalProtections;
     public List<DiscreteSignalProtection> DiscreteSignalProtections;
     public List<ExecutiveMechanism> ExecutiveMechanisms;
+    public string PathToProject;
 
     public App()
     {
-        var ai = new AnalogInput()
+        var ai = new AnalogInput
         {
             Name = "A1", HighLimit = 100, LowLimit = 0, HighAlarm = 90,
             LowAlarm = 10, HighWarning = 80, LowWarning = 20
         };
-        var di = new DiscreteInput()
+        var ai2 = new AnalogInput
+        {
+            Name = "A1", HighLimit = 100, LowLimit = 0, HighAlarm = 90,
+            LowAlarm = 10, HighWarning = 80, LowWarning = 20
+        };
+        var di = new DiscreteInput
         {
             Name = "Di"
         };
 
-        var do1 = new DiscreteOutput()
+        var do1 = new DiscreteOutput
         {
             Name = "Do1"
         };
 
-        var singleInput = new SingleInput()
+        var singleInput = new SingleInput
         {
             Name = "SingleInput", DiscreteInput = di
         };
 
-        var singleOutput = new SingleOutput()
+        var singleOutput = new SingleOutput
         {
             Name = "SingleOutput", DigitalOutput = do1
         };
 
-        var analogSignalProtection = new AnalogSignalProtection()
+        var analogSignalProtection = new AnalogSignalProtection
         {
-            AnalogInput = ai, IsUpperLimitProtection = true, Name = "Ap1"
+            AnalogInput = ai, IsUpperLimitProtection = false, Name = "Ap1"
         };
 
-        var discreteSignalProtection = new DiscreteSignalProtection()
+        var discreteSignalProtection = new DiscreteSignalProtection
         {
             IsOnLimitProtection = true, Name = "Dip1", SingleInput = singleInput
         };
 
-        var executiveMechanism = new ExecutiveMechanism()
+        var executiveMechanism = new ExecutiveMechanism
         {
             Name = "Exmech"
         };
 
-        AnalogInputs = new List<AnalogInput>(){ai};
-        DiscreteInputs = new List<DiscreteInput>(){di};
-        DiscreteOutputs = new List<DiscreteOutput>(){do1};
-        SingleInputs = new List<SingleInput>(){singleInput};
-        SingleOutputs = new List<SingleOutput>(){singleOutput};
-        AnalogSignalProtections = new List<AnalogSignalProtection>(){analogSignalProtection};
-        DiscreteSignalProtections = new List<DiscreteSignalProtection>(){discreteSignalProtection};
-        ExecutiveMechanisms = new List<ExecutiveMechanism>() {executiveMechanism};
+        AnalogInputs = new List<AnalogInput>
+            { ai, ai2 };
+        DiscreteInputs = new List<DiscreteInput>
+            { di };
+        DiscreteOutputs = new List<DiscreteOutput>
+            { do1 };
+        SingleInputs = new List<SingleInput>
+            { singleInput };
+        SingleOutputs = new List<SingleOutput>
+            { singleOutput };
+        AnalogSignalProtections = new List<AnalogSignalProtection>
+            { analogSignalProtection };
+        DiscreteSignalProtections = new List<DiscreteSignalProtection>
+            { discreteSignalProtection };
+        ExecutiveMechanisms = new List<ExecutiveMechanism>
+            { executiveMechanism };
 
         ChooseLocalConfigDirectory();
         MainWindow mainWindow = new();
@@ -81,35 +95,28 @@ public partial class App
 
     private void ChooseLocalConfigDirectory()
     {
-        var messageBoxResult =
-            MessageBox.Show("Do you have a configuration file?", "Hello!", MessageBoxButton.YesNo);
-        if (messageBoxResult != MessageBoxResult.Yes) return;
-
         OpenFileDialog openFileDialog = new()
         {
-            Filter = "Excel Files (*.xls, *.xlsx)|*.xls;*.xlsx|Text Files (*.txt)|*.txt"
+            ValidateNames = false,
+            CheckFileExists = false,
         };
 
         if (openFileDialog.ShowDialog() != true) return;
 
-        var extension = Path.GetExtension(openFileDialog.FileName);
-        var path = Path.GetFullPath(openFileDialog.FileName);
+        var path = Path.GetDirectoryName(openFileDialog.FileName);
 
-        switch (extension)
-        {
-            case ".xls" or ".xlsx":
-                CreateConfigByExcel();
-                break;
-            case ".txt":
-                WriteConfigToTxt(path);
-                break;
-        }
+        if (path == null)
+            return;
 
+        PathToProject = path;
         CreateConfigByTxt(path);
     }
 
     private void CreateConfigByTxt(string path)
     {
+        if (!File.Exists(path + "config.txt"))
+            return;
+
         try
         {
             var data = File.ReadAllText(path);
@@ -159,6 +166,5 @@ public partial class App
 
     private void CreateConfigByExcel()
     {
-
     }
 }
