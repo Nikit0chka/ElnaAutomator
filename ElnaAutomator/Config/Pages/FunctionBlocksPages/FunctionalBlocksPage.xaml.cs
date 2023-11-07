97ef2a4270d3daa7217d96c830fe560b97b60470
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-07 17:36:38 +0400

    Added event holder on button click

--- ElnaAutomator/Config/Pages/FunctionBlocksPages/FunctionalBlocksPage.xaml.cs
+++ ElnaAutomator/Config/Pages/FunctionBlocksPages/FunctionalBlocksPage.xaml.cs
@@ -1,9 +1,21 @@
-﻿namespace ElnaAutomator.Config.Pages.FunctionBlocksPages;
+﻿using System.Windows;
+using ElnaAutomator.Config.Generators;
+
+namespace ElnaAutomator.Config.Pages.FunctionBlocksPages;
 
 public partial class FunctionalBlocksPage
 {
+    private readonly App? _currentApp;
+
     public FunctionalBlocksPage()
     {
         InitializeComponent();
+        _currentApp = Application.Current as App;
+    }
+
+    private void ProcAiInit_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (_currentApp != null)
+            FunctionBlocksGenerator.GenerateProcAiInit(_currentApp.PathToProject, _currentApp.AnalogInputs);
     }
 }
