6ed74ae850923f2f316a8a4d201001a39b6b935b
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-07 17:35:44 +0400

    Added field "ModuleAddress"

--- ElnaAutomator/Config/ConfigStructs/AnalogInput.cs
+++ ElnaAutomator/Config/ConfigStructs/AnalogInput.cs
@@ -2,11 +2,12 @@
 
 public class AnalogInput
 {
-    public required string Name{ get; set; }
-    public required int HighLimit{ get; set; }
-    public required int LowLimit{ get; set; }
-    public int? HighAlarm{ get; set; }
-    public int? LowAlarm{ get; set; }
-    public int? HighWarning{ get; set; }
+    public required string Name { get; set; }
+    public required int HighLimit { get; set; }
+    public required int LowLimit { get; set; }
+    public required int ModuleAddress { get; set; }
+    public int? HighAlarm { get; set; }
+    public int? LowAlarm { get; set; }
+    public int? HighWarning { get; set; }
     public int? LowWarning { get; set; }
 }
