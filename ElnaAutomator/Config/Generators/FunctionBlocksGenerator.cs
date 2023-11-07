361378640dcc207ff537a54c4bb92c4fa7d00ed5
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-07 17:37:06 +0400

    Added logic for generating ProcAiInit

---
+++ ElnaAutomator/Config/Generators/FunctionBlocksGenerator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ElnaAutomator.Config.ConfigStructs;
+
+namespace ElnaAutomator.Config.Generators;
+
+public static class FunctionBlocksGenerator
+{
+    private const string FunctionBlocksFolderName = "FunctionBlocks";
+
+    public static void GenerateProcAiInit(string pathToProjectDirectory, List<AnalogInput> analogInputs)
+    {
+        StringBuilder content = new();
+
+        content.Append("FUNCTION_BLOCK Proc_AI_Init\n\n");
+        content.Append("VAR_INPUT\n\tinit : BOOL;\nEND_VAR\n\n");
+        content.Append("VAR\n\tstAI_Ini : fb_AI_Init;\n\tinits : UINT;\nEND_VAR\n\n");
+        content.Append("VAR_INPUT\n\tinitiales : UINT;\nEND_VAR\n\n");
+        content.Append("VAR_EXTERNAL\n\tAi : AiConfig;\nEND_VAR\n\n");
+
+        content.Append("inits := 0;\n\n");
+
+        foreach (var analogInput in analogInputs)
+        {
+            content.Append($"stAI_Ini(data_Ini := TRUE, dLL := {analogInput.LowAlarm}, dHL := {analogInput.HighAlarm},\n");
+            if (analogInput.LowWarning != null)
+                content.Append($"ISVALIDREF_LW := TRUE, dLW := {analogInput.LowWarning},\n");
+            if (analogInput.HighWarning != null)
+                content.Append($"ISVALIDREF_HW := TRUE, dHW := {analogInput.HighWarning},\n");
+            if (analogInput.LowAlarm != null)
+                content.Append($"ISVALIDREF_LA := TRUE, dLA := {analogInput.LowAlarm},\n");
+            if (analogInput.HighAlarm != null)
+                content.Append($"ISVALIDREF_HA := TRUE, dHA := {analogInput.HighAlarm},\n");
+
+            content.Append($"strAI := Ai.{analogInput.Name});\n");
+            content.Append("inits := inits + BOOL_TO_UINT(stAI_Ini.Init);\n\n");
+        }
+
+        content.Append("\ninitiales := 0;\nIF init THEN initiales := inits; END_IF;");
+
+        CreateFile($@"{pathToProjectDirectory}\{FunctionBlocksFolderName}\Proc_AI_Init.st", content);
+    }
+
+    private static void GenerateProcAi(string pathToProjectDirectory, List<AnalogInput> analogInputs)
+    {
+        StringBuilder content = new();
+
+        content.Append("FUNCTION_BLOCK Proc_AI\n\n");
+        content.Append("VAR\n\tinit : BOOL;\nEND_VAR\n\n");
+        content.Append("VAR\n\tstAI_Ini : fb_AI_Init;\n\tinits : UINT;\nEND_VAR\n\n");
+        content.Append("VAR_INPUT\n\tinitiales : UINT;\nEND_VAR\n\n");
+        content.Append("VAR_EXTERNAL\n\tAi : AiConfig;\nEND_VAR\n\n");
+
+        content.Append("inits := 0;\n\n");
+
+        foreach (var analogInput in analogInputs)
+        {
+            content.Append($"stAI_Ini(data_Ini := TRUE, dLL := {analogInput.LowAlarm}, dHL := {analogInput.HighAlarm},\n");
+            if (analogInput.LowWarning != null)
+                content.Append($"ISVALIDREF_LW := TRUE, dLW := {analogInput.LowWarning},\n");
+            if (analogInput.HighWarning != null)
+                content.Append($"ISVALIDREF_HW := TRUE, dHW := {analogInput.HighWarning},\n");
+            if (analogInput.LowAlarm != null)
+                content.Append($"ISVALIDREF_LA := TRUE, dLA := {analogInput.LowAlarm},\n");
+            if (analogInput.HighAlarm != null)
+                content.Append($"ISVALIDREF_HA := TRUE, dHA := {analogInput.HighAlarm},\n");
+
+            content.Append($"strAI := Ai.{analogInput.Name});\n");
+            content.Append("inits := inits + BOOL_TO_UINT(stAI_Ini.Init);\n\n");
+        }
+
+        content.Append("\ninitiales := 0;\nIF init THEN initiales := inits; END_IF;");
+
+        CreateFile($@"{pathToProjectDirectory}\{FunctionBlocksFolderName}\Proc_AI.st", content);
+    }
+
+    private static void CreateFile(string path, StringBuilder content)
+    {
+        try
+        {
+            File.WriteAllText(path, content.ToString());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
+    }
+}
