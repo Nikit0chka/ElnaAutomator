fc07ab2e4d31b42fdf0f55152c52b5a3da74d1bc
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-12 03:31:43 +0400

    Added logic for creating executiveMechanisms

--- ElnaAutomator/Config/ConfigStructs/ExecutiveMechanism.cs
+++ ElnaAutomator/Config/ConfigStructs/ExecutiveMechanism.cs
@@ -1,6 +1,18 @@
 namespace ElnaAutomator.Config.ConfigStructs;
 
-public class ExecutiveMechanism
+abstract public class ExecutiveMechanism
 {
     public required string Name {get; set; }
+    public DiscreteInput? StatOnDiscreteInput { get; set; }
+    public int? StatOnDiscreteInputBit { get; set; }
+    public DiscreteInput? StatOffDiscreteInput { get; set; }
+    public int? StatOffDiscreteInputBit { get; set; }
+    public DiscreteInput? InSoDiscreteInput { get; set; }
+    public int? InSoDiscreteInputBit { get; set; }
+    public DiscreteInput? InSzDiscreteInput { get; set; }
+    public int? InSzDiscreteInputBit { get; set; }
+    public DiscreteOutput? CmdOnDiscreteInput { get; set; }
+    public int? CmdOnDiscreteInputBit { get; set; }
+    public DiscreteOutput? CmdOffDiscreteInput { get; set; }
+    public int? CmdOffDiscreteInputBit { get; set; }
 }
