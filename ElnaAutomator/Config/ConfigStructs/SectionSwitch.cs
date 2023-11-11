b44ad3a453305a70051ebaf7ec2a54f12a8e3a38
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-12 03:35:00 +0400

    Added logic for creating sections switches

---
+++ ElnaAutomator/Config/ConfigStructs/SectionSwitch.cs
@@ -0,0 +1,9 @@
+﻿namespace ElnaAutomator.Config.ConfigStructs;
+
+public class SectionSwitch:ExecutiveMechanism
+{
+    public DiscreteInput? BasketInDiscreteInput { get; set; }
+    public int? BasketInDiscreteInputBit { get; set; }
+    public DiscreteInput? BasketOutDiscreteInput { get; set; }
+    public int? BasketOutDiscreteInputBit { get; set; }
+}
