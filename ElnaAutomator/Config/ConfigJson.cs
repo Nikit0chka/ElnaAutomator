fb9b81b46a7871a46dd799c4815dcb550f0e8b46
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-14 17:36:24 +0400

    Added fields "SingleInputs" "SingleOutputs" "AnalogSignalProtections", "SectionSwitches"

--- ElnaAutomator/Config/ConfigJson.cs
+++ ElnaAutomator/Config/ConfigJson.cs
@@ -9,13 +9,14 @@ namespace ElnaAutomator.Config;
 public class ConfigJson
 {
     public required List<AnalogInput> AnalogInputs { get; set; }
-    public required List<AnalogSignalProtection> AnalogSignalProtections { get; set; }
     public required List<DiscreteInput> DiscreteInputs { get; set; }
     public required List<DiscreteOutput> DiscreteOutputs { get; set; }
+    public required List<SingleInput> SingleInputs { get; set; }
+    public required List<SingleOutput> SingleOutputs { get; set; }
+    public required List<AnalogSignalProtection> AnalogSignalProtections { get; set; }
     public required List<DiscreteSignalProtection> DiscreteSignalProtections { get; set; }
     public required List<Kran> Krans { get; set; }
     public required List<Switch> Switches { get; set; }
     public required List<OilPump> OilPumps { get; set; }
-    public required List<SingleInput> SingleInputs { get; set; }
-    public required List<SingleOutput> SingleOutputs { get; set; }
+    public required List<SectionSwitch> SectionSwitches { get; set; }
 }
