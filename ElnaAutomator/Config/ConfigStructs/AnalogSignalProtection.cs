2da48ed21969afc942b33c365ec632f010d850c0
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-14 17:35:18 +0400

    Added logic for finding analog signal by name

--- ElnaAutomator/Config/ConfigStructs/AnalogSignalProtection.cs
+++ ElnaAutomator/Config/ConfigStructs/AnalogSignalProtection.cs
@@ -1,8 +1,21 @@
-﻿namespace ElnaAutomator.Config.ConfigStructs;
+﻿using System.Linq;
+using System.Text.Json.Serialization;
+using System.Windows;
+
+namespace ElnaAutomator.Config.ConfigStructs;
 
 public class AnalogSignalProtection
 {
+    [JsonIgnore] private readonly App _currentApp = (App) Application.Current;
     public required string Name { get; init; }
-    public  AnalogInput? AnalogInput { get; set; }
+    public string? AnalogInputName { get; set; }
+
+    [JsonIgnore]
+    public AnalogInput? AnalogInput
+    {
+        get => _currentApp.AnalogInputs.FirstOrDefault(ai => ai.Name == Name);
+        set => AnalogInputName = value?.Name;
+    }
+
     public required bool IsUpperLimitProtection { get; set; }
 }
