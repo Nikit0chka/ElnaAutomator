fc74a6c2a4c3ee049479b4dfd13469a6ab630d9b
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-17 16:16:12 +0400

    Added properties

--- ElnaAutomator/Config/ConfigStructs/SectionSwitch.cs
+++ ElnaAutomator/Config/ConfigStructs/SectionSwitch.cs
@@ -5,5 +5,11 @@ public class SectionSwitch:ExecutiveMechanism
     public DiscreteInput? BasketInDiscreteInput { get; set; }
     public int? BasketInDiscreteInputBit { get; set; }
     public DiscreteInput? BasketOutDiscreteInput { get; set; }
-    public int? BasketOutDiscreteInputBit { get; set; }
+    public int? BasketOutDiscreteInputBit { get; set; } 
+    public DiscreteInput? BasketTestDiscreteInput { get; set; }
+    public int? BasketTestDiscreteInputBit { get; set; }
+    public DiscreteInput? InBreakCmdOn { get; set; }
+    public int? InBreakCmdOnBit { get; set; } 
+    public DiscreteInput? InBreakCmdOff { get; set; }
+    public int? InBreakCmdOffBit { get; set; }
 }
