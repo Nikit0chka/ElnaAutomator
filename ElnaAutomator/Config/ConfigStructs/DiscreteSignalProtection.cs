a08fcffd3dc8d9e5c8dab49ef5d1a1651b6471fa
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-17 16:09:48 +0400

    Changed getters and setters

--- ElnaAutomator/Config/ConfigStructs/DiscreteSignalProtection.cs
+++ ElnaAutomator/Config/ConfigStructs/DiscreteSignalProtection.cs
@@ -1,16 +1,10 @@
-﻿using System.Linq;
-using System.Text.Json.Serialization;
-using System.Windows;
-
-namespace ElnaAutomator.Config.ConfigStructs;
+﻿namespace ElnaAutomator.Config.ConfigStructs;
 
 public class DiscreteSignalProtection
 {
-    [JsonIgnore] private readonly App _currentApp = (App) Application.Current;
-    public required string Name{ get; init; }
-    
-    public string? SingleInputName { get; set; }
-    public SingleInput? SingleInput { get => _currentApp.SingleInputs.FirstOrDefault(signal => signal.Name == SingleInputName);
-        set => SingleInputName = value?.Name; }
-    public required bool IsOnLimitProtection { get; set; }
+    public required string Name { get; init; }
+
+    public SingleInput? SingleInput { get; set; }
+    public required int Delay { get; set; }
+    public required bool IsRunOnStart { get; set; }
 }
