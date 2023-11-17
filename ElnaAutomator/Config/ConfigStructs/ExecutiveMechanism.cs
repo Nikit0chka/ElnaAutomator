05af2b958761ab64386b289d17c4854af941627b
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-17 16:11:16 +0400

    Changed fields

--- ElnaAutomator/Config/ConfigStructs/ExecutiveMechanism.cs
+++ ElnaAutomator/Config/ConfigStructs/ExecutiveMechanism.cs
@@ -7,10 +7,10 @@ abstract public class ExecutiveMechanism
     public int? StatOnDiscreteInputBit { get; set; }
     public DiscreteInput? StatOffDiscreteInput { get; set; }
     public int? StatOffDiscreteInputBit { get; set; }
-    public DiscreteInput? InSoDiscreteInput { get; set; }
-    public int? InSoDiscreteInputBit { get; set; }
-    public DiscreteInput? InSzDiscreteInput { get; set; }
-    public int? InSzDiscreteInputBit { get; set; }
+    public DiscreteInput? StatOnReabDiscreteInput { get; set; }
+    public int? StatOnReabDiscreteInputBit { get; set; }
+    public DiscreteInput? StatOffReabDiscreteInput { get; set; }
+    public int? StatOffReabDiscreteInputBit { get; set; }
     public DiscreteOutput? CmdOnDiscreteOutput { get; set; }
     public int? CmdOnDiscreteOutputBit { get; set; }
     public DiscreteOutput? CmdOffDiscreteOutput { get; set; }
