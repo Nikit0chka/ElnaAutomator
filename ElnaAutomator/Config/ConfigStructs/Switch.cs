5d47a5248625b613d987e2074f6d7a480d139f5b
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-17 16:18:36 +0400

    Added properties

--- ElnaAutomator/Config/ConfigStructs/Switch.cs
+++ ElnaAutomator/Config/ConfigStructs/Switch.cs
@@ -2,4 +2,8 @@ namespace ElnaAutomator.Config.ConfigStructs;
 
 public class Switch:ExecutiveMechanism
 {
+    public DiscreteInput? InBreakCmdOn { get; set; }
+    public int? InBreakCmdOnBit { get; set; }
+    public DiscreteInput? InBreakCmdOff { get; set; }
+    public int? InBreakCmdOffBit { get; set; }
 }
