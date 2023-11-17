9d601d7b38c8f96433b5898b661c1f3797c48b0f
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-17 16:18:10 +0400

    Made property "Address" non nullable

--- ElnaAutomator/Config/ConfigStructs/SingleOutput.cs
+++ ElnaAutomator/Config/ConfigStructs/SingleOutput.cs
@@ -3,6 +3,6 @@
 public class SingleOutput
 {
     public required string Name { get; set; }
-    public DiscreteOutput? DigitalOutput { get; set; }
-    public int Address { get; set; }
+    public DiscreteOutput? DiscreteOutput { get; set; }
+    public int? Address { get; set; }
 }
