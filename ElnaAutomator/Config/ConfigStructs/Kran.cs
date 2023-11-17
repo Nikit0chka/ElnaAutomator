675d6a0841bb5b34335992ec0b19f35c72322322
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-17 16:14:08 +0400

    Added properties

--- ElnaAutomator/Config/ConfigStructs/Kran.cs
+++ ElnaAutomator/Config/ConfigStructs/Kran.cs
@@ -1,5 +1,12 @@
 namespace ElnaAutomator.Config.ConfigStructs;
 
-public class Kran : ExecutiveMechanism
+public class Kran:ExecutiveMechanism
 {
+    public DiscreteInput? InDpDiscreteInput { get; set; }
+    public int? InDpDiscreteInputBit { get; set; }
+    public DiscreteInput? InSoDiscreteInput { get; set; }
+    public int? InSoDiscreteInputBit { get; set; }
+    public DiscreteInput? InSzDiscreteInput { get; set; }
+    public int? InSzDiscreteInputBit { get; set; }
+
 }
