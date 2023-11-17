282da993498655555ac0ccd5a99895b9f3dc2f19
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-17 16:16:48 +0400

    Deleted property "IsInversed"

--- ElnaAutomator/Config/ConfigStructs/SingleInput.cs
+++ ElnaAutomator/Config/ConfigStructs/SingleInput.cs
@@ -4,5 +4,6 @@ public class SingleInput
 {
     public required string Name { get; set; }
     public DiscreteInput? DiscreteInput { get; set; }
-    public int Address { get; set; }
+    public required bool IsInversed { get; set; }
+    public int? Address { get; set; }
 }
