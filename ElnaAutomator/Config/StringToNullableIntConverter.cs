4f62ae183050b26ac3cc43d40e8abeef3c8001d7
Nikit0chka <voronsov.nikita19742004@gmail.com>
2023-11-21 17:11:03 +0400

    Added logic for converting empty string to int?

---
+++ ElnaAutomator/Config/StringToNullableIntConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ElnaAutomator.Config;
+
+//Костыль для сброса значения сигнала в null
+public class StringToNullableIntConverter:IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value?.ToString();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (string.IsNullOrEmpty((string) value!))
+            return null;
+
+        return !int.TryParse((string) value, out var result)? DependencyProperty.UnsetValue : result;
+    }
+}
